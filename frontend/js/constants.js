dfff957628ce0922f2d4ccc1e6f70578918888f3
Savvy Shero Club <109817119+SavvySheroClub@users.noreply.github.com>
2022-08-23 13:42:32 -0400

    update text

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the Shero Club NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = "The Shero Club NFT Project is a shoutout to amazing everyday sheroes";
+const welcome_p = "The Shero Club NFT Project is a shoutout to amazing everyday sheroes.";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
